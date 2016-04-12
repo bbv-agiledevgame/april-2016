23830ed31d5d6ec18e31a9fc08dcccffc0d1f1cd
Daniel Fahrni <dafahrni@hotmail.com>
2016-04-12 14:25:38 +0200

    initial implementation of BoardView

---
+++ Source/Winkeladvokat/BoardView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Winkeladvokat
+{
+    /// <summary>
+    /// Interaction logic for BoardView.xaml
+    /// </summary>
+    public partial class BoardView : UserControl
+    {
+        public BoardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
