4b88e232698f958be41d173dc2dde440a26e9fb9
Michael Estermann <mike@nastuechli.ch>
2016-04-13 09:43:50 +0200

    Converter implemented hide the numbers of corner fields

---
+++ Source/Winkeladvokat/Converter/FieldValueToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Winkeladvokat.Converter
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class FieldValueToVisibilityConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int numericValue = System.Convert.ToInt32(value);
+
+            return numericValue != 0 ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
