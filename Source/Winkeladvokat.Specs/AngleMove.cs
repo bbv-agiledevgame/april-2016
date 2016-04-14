07d5461b51a42bdbc993c569630866a730c7da6f
Muhamed Ahmetovic <muhamed.ahmetovic@uzh.ch>
2016-04-14 16:06:54 +0200

    Added spec test for AngleMove

---
+++ Source/Winkeladvokat.Specs/AngleMove.cs
@@ -0,0 +1,41 @@
+﻿namespace Winkeladvokat
+{
+    using FluentAssertions;
+    using Models;
+    using ViewModels;
+    using Xbehave;
+
+    public class AngleMove
+    {
+        [Scenario]
+        public void RegularMove(BoardViewModel boardViewModel)
+        {
+            "establish board"._(() =>
+            {
+                boardViewModel = new BoardViewModel();
+            });
+
+            "when first player moves valid angle"._(() =>
+            {
+                boardViewModel.SelectField.Execute(boardViewModel.Fields[0][6]);
+                boardViewModel.SelectField.Execute(boardViewModel.Fields[7][6]);
+            });
+
+            "it should put current player token on new field"._(() =>
+            {
+                boardViewModel.Fields[7][6].Token.Should().BeOfType<AdvocateToken>();
+                boardViewModel.Fields[7][6].Token.Player.Should().Be(boardViewModel.Players[0]);
+            });
+
+            "it should clear token on player's previous field"._(() =>
+            {
+                boardViewModel.Fields[0][0].Token.Should().BeNull();
+            });
+
+            "it should give turn to player 2"._(() =>
+            {
+                boardViewModel.CurrentPlayer.Should().Be(boardViewModel.Players[1]);
+            });
+        }
+    }
+}
