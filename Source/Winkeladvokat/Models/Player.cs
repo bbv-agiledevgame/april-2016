8b5a2fa69f0c7cc987936becee768453d67fb637
onizuka87 <onizuka87@gmail.com>
2016-04-14 17:03:28 +0200

    Setted token brush color.

--- Source/Winkeladvokat/Models/Player.cs
+++ Source/Winkeladvokat/Models/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 using PropertyChanged;
 
 namespace Winkeladvokat.Models
@@ -6,10 +7,15 @@ namespace Winkeladvokat.Models
     [ImplementPropertyChanged]
     public class Player
     {
-        public Player(Point position)
+        public Player(Point position, Color color)
         {
             this.Position = position;
+            this.Color = color; 
         }
+
+        public Color Color { get; private set; }
+
+        public Brush FillBrush { get { return new SolidColorBrush(this.Color); } }
 
         public Point Position { get; set; }
     }
