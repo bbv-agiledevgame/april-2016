206f0ad603132d81b28b5b33d64e2bc5f4b32aac
onizuka87 <onizuka87@gmail.com>
2016-04-15 11:12:29 +0200

    Merging pull request with new Token model

--- Source/Winkeladvokat.Facts/Services/MovementFinderTest.cs
+++ Source/Winkeladvokat.Facts/Services/MovementFinderTest.cs
@@ -29,7 +29,7 @@
         {
             Player player = new Player(Colors.Transparent);
             BoardField selectedField1 = new BoardField(0, default(Brush), new Position(1, 1));
-            selectedField1.Token = new ParagraphToken(player);
+            selectedField1.Token = new Token(TokenType.Paragraph, player);
             BoardField selectedField2 = new BoardField(0, default(Brush), new Position(1,2));
             
             Board board = new Board(new List<List<BoardField>>(new[] { new List<BoardField> { selectedField1, selectedField2 } }));
